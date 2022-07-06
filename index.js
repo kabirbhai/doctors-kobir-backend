2d7e188410ba8cee6812d1188b2ed327c115eea1
kabir <makkobir79@gmail.com>
2022-07-06 19:21:23 +0600

    services api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-require("dotenv").config;
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -9,21 +9,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nddtz.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nddtz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  console.log("connected");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("connected");
+    const servicesCollection = client
+      .db("Doctors-kobir")
+      .collection("Services");
+
+    app.get("/service", async (req, res) => {
+      const services = await servicesCollection.find({}).toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello there Kabir doctor is lessening");
+  res.send("hello world");
 });
 
 app.listen(port, () => {
