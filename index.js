e2c115edd343e275991388a7169560c010682301
kabir <makkobir79@gmail.com>
2022-07-07 16:32:43 +0600

    user api created

--- index.js
+++ index.js
@@ -21,6 +21,20 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db("Doctors-kobir").collection("Services");
     const bookingCollection = client.db("Doctors-kobir").collection("Booking");
+    const userCollection = client.db("Doctors-kobir").collection("Users");
+
+    // USER COLLECTION API
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
 
     // LOAD ALL SERVICE
     app.get("/service", async (req, res) => {
@@ -29,12 +43,19 @@ async function run() {
     });
 
     // BOOKING SERVICE
+    app.get("/booking", async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/booking", async (req, res) => {
       const booking = req.body;
       const filter = {
         treatment: booking.treatment,
         date: booking.date,
-        patent: booking.patent,
+        patient: booking.patient,
       };
       const exists = await bookingCollection.findOne(filter);
       if (exists) {
@@ -42,6 +63,36 @@ async function run() {
       }
       const result = await bookingCollection.insertOne(booking);
       return res.send({ success: true, result });
+    });
+    // -------------------------
+    // warning !!
+    // ======================
+    // This is not the proper way to query after learning more mongoDB you have to lookup aggregation pipeline
+    // AVAILABLE
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+      // step 1 get all data
+      const services = await serviceCollection.find().toArray();
+
+      //step 2 get the booking of the day
+      const filter = { date: date };
+      const bookings = await bookingCollection.find(filter).toArray();
+
+      // step 3 for each service
+      services.forEach((service) => {
+        // step 4 find booking for that service
+        const serviceBooking = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        //step 5 select slots for the service booking
+        const bookedSlots = serviceBooking.map((book) => book.slot);
+        //step 6 select those slot that are not include in BookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        service.slots = available;
+      });
+      res.send(services);
     });
   } finally {
   }
